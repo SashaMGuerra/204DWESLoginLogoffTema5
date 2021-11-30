85a8c26253bb802c3737d4dd5c7a2f31e87d0df6
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-11-30 13:54:28 +0100

    30/11/2021 login y programa
    login funcional
    programa ahí está

--- codigoPHP/login.php
+++ codigoPHP/login.php
@@ -9,6 +9,9 @@
  * Comprueba si la combinación usuario-contraseña introducidos existen en la base
  * de datos, y si no es así, lo pide de nuevo.
  */
+/* Inicio de la sesión para almacenar el código de usuario */
+session_start();
+
 require_once '../core/libreriaValidacion.php'; // Librería de validación.
 require_once '../config/configApp.php'; // Constantes de validación.
 require_once '../config/configDB.php'; // Constantes de conexión a la base de datos.
@@ -19,49 +22,53 @@ $aFormulario = [
     'password' => ''
 ];
 
-// Variable de error. Si al enviar el formulario existe alguno, lo muestra.
-$sError = '';
-
+/**
+ * Si se ha enviado el formulario, valida la entrada.
+ */
 if (isset($_REQUEST['submit'])) {
     // Manejador de errores. 
     $bEntradaOK = true;
 
     /*
-     * Validación de entrada.
+     * Si el usuario y/o password no está definido, o si se ha introducido de
+     * forma incorrecta, pone la entrada como incorrecta.
      */
     if (validacionFormularios::comprobarAlfaNumerico($_REQUEST['usuario'], 8, 4, OBLIGATORIO) || validacionFormularios::comprobarAlfaNumerico($_REQUEST['password'], 8, 4, OBLIGATORIO)) {
-        $sError = "Debes introducir un nombre de usuario y una contraseña válidos";
         $bEntradaOK = false;
     }
-}
 
-/*
- * Si el formulario no ha sido enviado, pone el manejador de errores
- * a false para poder mostrar el formulario.
- */ else {
-    $bEntradaOK = false;
-}
+    /**
+     * Si no existe ningún error por el momento, comprueba que el usuario y la
+     * contraseña existan y sean correctos en la base de datos.
+     */
+    if ($bEntradaOK) {
+        /* Recogida de información */
+        $aFormulario['usuario'] = $_REQUEST['usuario'];
+        $aFormulario['password'] = $_REQUEST['password'];
 
-/**
- * Si la entrada es correcta, se conecta a la base de datos y comprueba que
- * el usuario y contraseña sean correctos.
- */
-if ($bEntradaOK) {
-    /*Recogida de información*/
-    $aFormulario['usuario'] = $_REQUEST['usuario'];
-    $aFormulario['password'] = $_REQUEST['password'];
-    
-    try {
-        // Conexión con la base de datos.
-        $oDB = new PDO(HOST, USER, PASSWORD);
-        $oDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            // Conexión con la base de datos.
+            $oDB = new PDO(HOST, USER, PASSWORD);
+            $oDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // Query de selección.
-        $sSelect = "SELECT T01_Password FROM T01_Usuario WHERE T01_CodUsuario='{$aFormulario['usuario']}'";
+            // Query de selección.
+            $sSelect = "SELECT T01_Password FROM T01_Usuario WHERE T01_CodUsuario='{$aFormulario['usuario']}'";
 
-        // Preparación y ejecución de la consulta.
-        $oResultadoSelect = $oDB->prepare($sSelect);
-        $oResultadoSelect->execute();
+            // Preparación y ejecución de la consulta.
+            $oResultadoSelect = $oDB->prepare($sSelect);
+            $oResultadoSelect->execute();
+
+            $oResultado = $oResultadoSelect->fetchObject();
+        } catch (PDOException $exception) {
+            /*
+             * Mostrado del código de error y su mensaje.
+             */
+            echo '<div>Se han encontrado errores:</div><ul>';
+            echo '<li>' . $exception->getCode() . ' : ' . $exception->getMessage() . '</li>';
+            echo '</ul>';
+        } finally {
+            unset($oDB);
+        }
 
         /*
          * Si el select no devuelve ningún resultado, es decir, el usuario no
@@ -72,31 +79,48 @@ if ($bEntradaOK) {
          * el comando hash para codificar la introducida y comprobar si son
          * la misma.
          */
-        $oResultado = $oResultadoSelect->fetchObject();
         if (!$oResultado || $oResultado->T01_Password != hash('sha256', ($aFormulario['usuario'] . $aFormulario['password']))) {
-            $sError = "Debes introducir un nombre de usuario y una contraseña válidos";
             $bEntradaOK = false;
         }
-        /**
-         * Si todo es correcto, actualiza la tabla para indicar la última conexión.
-         */ else {
-            /**
-             * Si usuario y contraseña son correctos, añade la conexión al registro
-             * de conexiones, y añade la última hora de conexión.
-             */
-            $oDateTime = new DateTime();
+    }
+}
+/*
+ * Si el formulario no ha sido enviado, pone el manejador de errores
+ * a false para poder mostrar el formulario.
+ */
+else {
+    $bEntradaOK = false;
+}
 
-            // Query de actualización.
-            $sUpdate = <<<QUERY
+/**
+ * Si la entrada es correcta, crea la variable de sesión que almacena el usuario.
+ * Además, se conecta a la base de datos y actualiza la tabla para indicar 
+ * la última conexión.
+ * Finalmente pasa al usuario a la página de programa.php
+ */
+if ($bEntradaOK) {
+    // Variable de sesión para el usuario.
+    $_SESSION['usuarioDAW204AppLoginLogoff'] = $aFormulario['usuario'];
+    
+    // Añadido al registro de conexiones y última hora de conexión.
+    try {
+        // Conexión con la base de datos.
+        $oDB = new PDO(HOST, USER, PASSWORD);
+        $oDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Fecha-hora actual.
+        $oDateTime = new DateTime();
+
+        // Query de actualización.
+        $sUpdate = <<<QUERY
                     UPDATE T01_Usuario SET T01_NumConexiones=T01_NumConexiones+1,
                     T01_FechaHoraUltimaConexion = '{$oDateTime->format("y-m-d h:i:s")}'
                     WHERE T01_CodUsuario='{$aFormulario['usuario']}'
             QUERY;
 
-            // Preparación y ejecución de la actualización.
-            $oResultadoUpdate = $oDB->prepare($sUpdate);
-            $oResultadoUpdate->execute();
-        }
+        // Preparación y ejecución de la actualización.
+        $oResultadoUpdate = $oDB->prepare($sUpdate);
+        $oResultadoUpdate->execute();
     } catch (PDOException $exception) {
         /*
          * Mostrado del código de error y su mensaje.
@@ -107,6 +131,10 @@ if ($bEntradaOK) {
     } finally {
         unset($oDB);
     }
+
+    // Reenvío al usuario a la página de programa.
+    header('Location: programa.php');
+    exit;
 }
 ?>
 <html>
@@ -165,7 +193,7 @@ if ($bEntradaOK) {
     </head>
     <body>
         <header>
-            <?php include_once './elementoBtVolver.php'; // Botón de regreso       ?>
+            <?php include_once './elementoBtVolver.php'; // Botón de regreso         ?>
             <h1>Acceso a la aplicación</h1>
         </header>
         <main>
@@ -179,11 +207,10 @@ if ($bEntradaOK) {
                         <li><label class="obligatorio" for='password' >Contraseña</label></li>
                         <li><input class="obligatorio" type='password' name='password' id='password'/></li>
                     </ul>
-                    <div class="error"><?php echo $sError ?? ''; ?></div>
                 </fieldset>
                 <input type='submit' name='submit' value='Entrar'/>
             </form>
         </main>
-            <?php include_once './elementoFooter.php'; //Footer    ?>
+        <?php include_once './elementoFooter.php'; //Footer      ?>
     </body>
 </html>
